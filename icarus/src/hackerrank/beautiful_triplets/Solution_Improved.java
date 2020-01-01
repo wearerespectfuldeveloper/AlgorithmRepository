485404b0da19bc00590091b247db37c0cdca4c9d
icarus8050 <icarus8050@naver.com>
2020-01-01 16:43:19 +0900

    Beautiful Triplets (https://www.hackerrank.com/challenges/beautiful-triplets/problem) 개선된 코드 (Hash Map 을 이용하여 O(n) 시간 복잡도로 해결 가능)

---
+++ icarus/src/hackerrank/beautiful_triplets/Solution_Improved.java
@@ -0,0 +1,55 @@
+package hackerrank.beautiful_triplets;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution_Improved {
+
+    // Complete the beautifulTriplets function below.
+    static int beautifulTriplets(int d, int[] arr) {
+        int ans = 0;
+
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i : arr) {
+            if (!map.containsKey(i)) {
+                map.put(i, 1);
+            } else {
+                map.put(i, map.get(i) + 1);
+            }
+
+            if (map.containsKey(i - d) && map.containsKey(i - (d * 2))) {
+                ans += map.get(i - d) * map.get(i - (d * 2));
+            }
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String[] nd = br.readLine().split(" ");
+
+        int n = Integer.parseInt(nd[0]);
+        int d = Integer.parseInt(nd[1]);
+
+        int[] arr = new int[n];
+
+        String[] arrItems = br.readLine().split(" ");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(arrItems[i]);
+        }
+
+        int result = beautifulTriplets(d, arr);
+
+        bw.write(String.valueOf(result));
+        bw.newLine();
+
+        bw.close();
+        br.close();
+    }
+}
